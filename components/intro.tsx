63bef6c4c38602b7a0d476ee1ea13245d0d7018e
Mudassir Ghazi <54005233+madyrock@users.noreply.github.com>
2026-06-29 17:18:25 +0530

    Update intro.tsx
    ok

--- components/intro.tsx
+++ components/intro.tsx
@@ -63,8 +63,8 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Mudassir.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">4 years</span> of experience. I enjoy
+        <span className="font-bold">front-end developer</span> with{" "}
+        <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
